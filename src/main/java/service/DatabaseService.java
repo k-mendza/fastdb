10d8c53996021a12a5e2bc893c89d61f0b3806c3
Karol Mendza <karolmendza@gmail.com>
2019-10-23 22:13:20 +0200

    Initial commit

---
+++ src/main/java/service/DatabaseService.java
@@ -0,0 +1,9 @@
+package service;
+
+public class DatabaseService {
+    public static final String DATABASE_HOST = "localhost";
+    public static final int DATABASE_PORT = 5432;
+    public static final String DATABASE_NAME = "postgres";
+    public static final String DATABASE_USER = "user";
+    public static final String DATABASE_PASSWORD = "password";
+}
