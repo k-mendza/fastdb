746b3d2b2dd60bc02454c7399f03319d351bee70
Karol Mendza <karolmendza@gmail.com>
2019-10-23 22:54:30 +0200

    Connection with docker container established

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -4,7 +4,7 @@ public class DatabaseConfig {
     public static final String DATABASE_HOST = "localhost";
     public static final int DATABASE_PORT = 5432;
     public static final String DATABASE_NAME = "postgres";
-    public static final String DATABASE_USER = "user";
+    public static final String DATABASE_USER = "postgres";
     public static final String DATABASE_PASSWORD = "password";
 
     public static String getConnectionURL() {
--- src/main/java/service/DatabaseService.java
+++ src/main/java/service/DatabaseService.java
@@ -12,12 +12,14 @@ public class DatabaseService {
     private static Logger logger = LoggerFactory.getLogger(DatabaseService.class);
 
     public static Connection openConnection() throws SQLException {
+        logger.info("Connection opened successfully");
         return DriverManager.getConnection(DatabaseConfig.getConnectionURL(), DatabaseConfig.DATABASE_USER, DatabaseConfig.DATABASE_PASSWORD);
     }
 
     public static void closeConnection(Connection connection) {
         try {
             connection.close();
+            logger.info("Connection closed successfully");
         } catch (Exception e) {
             logger.error("Error while closing connection to database");
             logger.error(e.getMessage());
